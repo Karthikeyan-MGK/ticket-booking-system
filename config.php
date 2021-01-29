18fce89ce7d835dcbdf65270e85b839e48a8e492
Karthikeyan-MGK <78212551+Karthikeyan-MGK@users.noreply.github.com>
2021-01-29 20:33:03 +0530

    have updated

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
     ob_start();
     session_start();
     $timezone = date_default_timezone_set("Asia/Calcutta");
-    
+
     $con = mysqli_connect("remotemysql.com", "82WaZSIa8q", " XS0ed0pI6k","82WaZSIa8q");
 
     if(mysqli_connect_errno())
